3db9f5b80c9fff5e8e66f632f6322522cde46e68
Alexander Markov <apsheronets@gmail.com>
2009-12-19 03:38:25 +0300

    documentation
    Ignore-this: 117ec18f8bab225ff59f1ca713c7f92d
    
    darcs-hash:20091219003825-c9899-6d36d16e495d91a6aef5a1ed34c88f91ca98825f

--- textile.mli
+++ textile.mli
@@ -26,41 +26,51 @@
 (** {9 Textile syntax tree} *)
 
 type attr =
-  | Class    of string (* p(myclass). *)
-  | Id       of string (* p(#myid). *)
-  | Style    of string (* p{color:red}. *)
-  | Language of string (* p[fr-fr]. *)
+  | Class    of string (** p(myclass). *)
+  | Id       of string (** p(#myid). *)
+  | Style    of string (** p{color:red}. *)
+  | Language of string (** p\[fr-fr\]. *) (* without backslashes :) *)
+
+(** Phrases may be presents like HTML tags for text formatting. For
+example, **ocaml is __functional__ language** is equivalent for <b>ocaml
+is <i>functional</i> language</b> or [Bold [CData "ocaml is "; Italic
+[CData "functional"]; CData " language"]] *)
 type phrase =
   | CData       of string
-  | Emphasis    of phrase list (* _ *)
-  | Strong      of phrase list (* * *)
-  | Italic      of phrase list (* __ *)
-  | Bold        of phrase list (* ** *)
-  | Citation    of phrase list (* ?? *)
-  | Deleted     of phrase list (* - *)
-  | Inserted    of phrase list (* + *)
-  | Superscript of phrase list (* ^ *)
-  | Subscript   of phrase list (* ~ *)
-  | Span        of phrase list (* % *)
-  | Code        of phrase list (* @ *)
-  | Acronym of string * string (* ABC(Always Be Closing *)
-  | Link of string * phrase    (* "linktext":url *)
+  | Emphasis    of phrase list (** _ *)
+  | Strong      of phrase list (** * *)
+  | Italic      of phrase list (** __ *)
+  | Bold        of phrase list (** ** *)
+  | Citation    of phrase list (** ?? *)
+  | Deleted     of phrase list (** - *)
+  | Inserted    of phrase list (** + *)
+  | Superscript of phrase list (** ^ *)
+  | Subscript   of phrase list (** ~ *)
+  | Span        of phrase list (** % *)
+  | Code        of phrase list (** @ *)
+  | Acronym of string * string (** ABC(Always Be Closing *)
+  | Link of string * phrase    (** "linktext":url *)
+
+(** One line of text. It terminates by line break character. *)
 type line =
   phrase list
+
+(** Alignment option. *)
 type align =
-  | Right   (* > *)
-  | Left    (* < *)
-  | Center  (* = *)
-  | Justify (* <> *)
+  | Right   (** > *)
+  | Left    (** < *)
+  | Center  (** = *)
+  | Justify (** <> *)
+
 type block =
-  | Header     of int * (attr list * align option * line list) (* h1. *)
-  | Blockquote of (attr list * align option * line list) (* bq. *)
-  | Footnote   of int * (attr list * align option * line list) (* fnn. *) (* FIXME *)
-  | Paragraph  of (attr list * align option * line list) (* p. *)
-  | Blockcode  of (attr list * align option * line list) (* bc. *)
-  | Pre        of (attr list * align option * line list) (* pre. *)
-  | Numlist    of (attr list * align option * line list) (* # *)
-  | Bulllist   of (attr list * align option * line list) (* * *)
+  | Header     of int * (attr list * align option * line list) (** h1. *)
+  | Blockquote of (attr list * align option * line list)       (** bq. *)
+  | Footnote   of int * (attr list * align option * line list) (** fnn. *) (* FIXME *)
+  | Paragraph  of (attr list * align option * line list) (** p. *)
+  | Blockcode  of (attr list * align option * line list) (** bc. *)
+  | Pre        of (attr list * align option * line list) (** pre. *)
+  | Numlist    of (attr list * align option * line list) (** # *)
+  | Bulllist   of (attr list * align option * line list) (** * *)
   (*| Table of FIXME *)
 
 

a0a3c7a3ab479fabbe4083eb270a3fb1774a09e2
Alexander Markov <apsheronets@gmail.com>
2009-12-23 02:46:41 +0300

    copyrights
    Ignore-this: c27955552a232fe85b9250667f10954d
    
    darcs-hash:20091222234641-c9899-e9b3e7be4a3845c214a82d9fa76c4d7451c9ea8e

--- textile.mli
+++ textile.mli
@@ -1,3 +1,20 @@
+(* This file is part of textile-ocaml.
+ *
+ * textile-ocaml is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * textile-ocaml is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with textile-ocaml.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2009 Alexander Markov *)
+
 (** Textile markup language support for OCaml
 
   @see
